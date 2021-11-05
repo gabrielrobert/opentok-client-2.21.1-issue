71abb9a31d5921f230ae1e72d959d3f004bc8339
Gabriel Robert <g.robert092@gmail.com>
2021-11-05 08:35:49 -0400

    Provide easy reproduction steps

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # opentok-client-2.21.1-issue
+
+## Installation
+
+```
+$ npm install
+$ npm run test
+```
+
+## Expected error
+![Error](issue_screenshot.png)
+
---
+++ index.js
@@ -0,0 +1,7 @@
+fixture`opentok`
+    .page('https://opentok.github.io/opentok-web-samples/Basic%20Video%20Chat/');
+
+test('Try opentok', async () => {
+    // an error should pop in your bash console
+    await t.wait(5000);
+});
